fe96a52720c2f37e32f7666d45c1d92ac95b14e6
Hydriuk <hydriuk@gmail.com>
2023-05-30 22:58:32 +0200

    Adapt PlayerKeysController for OpenMod

--- UnturnedModules/PlayerKeys/PlayerKeysController.cs
+++ UnturnedModules/PlayerKeys/PlayerKeysController.cs
@@ -1,12 +1,20 @@
-﻿using SDG.Unturned;
+﻿#if OPENMOD
+using Microsoft.Extensions.DependencyInjection;
+using OpenMod.API.Ioc;
+#endif
+using SDG.Unturned;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Protocols.WSTrust;
 using System.Linq;
 using System.Text;
 using UnityEngine.UIElements;
 
 namespace Hydriuk.UnturnedModules.PlayerKeys
 {
+#if OPENMOD
+    [PluginServiceImplementation(Lifetime = ServiceLifetime.Singleton)]
+#endif
     public class PlayerKeysController : IPlayerKeysController
     {
         public PlayerKeysController() 
@@ -29,7 +37,7 @@ namespace Hydriuk.UnturnedModules.PlayerKeys
             {
                 player.gameObject
                     .GetComponent<PlayerKeysListener>()
-                    .Dispose();
+                    ?.Dispose();
             }
         }
 
