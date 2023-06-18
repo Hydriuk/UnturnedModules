5ee3a86520f9714009c88f28dbb5277ee8803f5e
Hydriuk <hydriuk@gmail.com>
2023-06-18 20:49:33 +0200

    Added Vector3Extensions

---
+++ UnturnedModules/Extensions/Vector3Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Hydriuk.UnturnedModules.Extensions
+{
+    public static class Vector3Extensions
+    {
+        public static float[] Serialize(this Vector3 vector)
+        {
+            return new float[] { vector.x, vector.y, vector.z };
+        }
+
+        public static Vector3 Deserialize(this float[] vector)
+        {
+            return new Vector3(vector[0], vector[1], vector[2]);
+        }
+    }
+}
