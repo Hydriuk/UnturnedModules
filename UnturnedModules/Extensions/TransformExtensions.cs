a6e3c4b8cdce382064c54a4d42ab1d80223a4fa9
Hydriuk <hydriuk@gmail.com>
2023-12-03 14:56:45 +0100

    Add transform extensions

---
+++ UnturnedModules/Extensions/TransformExtensions.cs
@@ -0,0 +1,28 @@
+﻿using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Hydriuk.UnturnedModules.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static BarricadeDrop? ToBarricade(this Transform transform)
+        {
+            BarricadeDrop drop = BarricadeManager.FindBarricadeByRootTransform(transform);
+
+            if (drop != null)
+                return drop;
+
+            InteractableDoorHinge doorHinge = transform.GetComponent<InteractableDoorHinge>();
+
+            return BarricadeManager.FindBarricadeByRootTransform(doorHinge?.door?.transform);
+        }
+
+        public static StructureDrop? ToStructure(this Transform transform)
+        {
+            return StructureManager.FindStructureByRootTransform(transform);
+        }
+    }
+}
